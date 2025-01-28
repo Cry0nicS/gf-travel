231518420966aaff107efa6e04225989c173afae
Adrian Popescu <adrianpopescu90@gmail.com>
2025-01-28 23:29:24 +0100

    Add Shadcn library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,17 @@ export default defineNuxtConfig({
     future: {
         compatibilityVersion: 4
     },
-    modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"],
+    modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxt/eslint", "@nuxtjs/color-mode"],
+    shadcn: {
+        /**
+         * Directory that the component lives in.
+         */
+        componentDir: "./app/components/ui",
+        /**
+         * Prefix for all the imported component
+         */
+        prefix: ""
+    },
     ssr: true, // Used by modules like nuxt-image for static image generation
     typescript: {
         typeCheck: true
